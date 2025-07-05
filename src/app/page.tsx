99d3cd0be9a022fede425a8db98f21adb7db3ff7
nitininhouse <nitin_g@me.iitr.ac.in>
2025-07-05 23:41:46 +0530

    feat:boarding page basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,4 +11,22 @@ const ZoraChanLanding = () => {
     }, 3000);
     return () => clearInterval(interval);
   }, []);
+  return (
+    <div className="min-h-screen bg-white text-black font-mono">
+      
+      <div className="relative">
+       
+        <div className="min-h-screen bg-white relative overflow-hidden border-8 border-black">
+         
+          <div 
+            className="absolute inset-0 opacity-20"
+            style={{
+              backgroundImage: `radial-gradient(circle, black 1px, transparent 1px)`,
+              backgroundSize: '20px 20px'
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
