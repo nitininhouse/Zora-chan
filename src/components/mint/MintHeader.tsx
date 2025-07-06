bfbf7d87954b099dd3dee9fadd3cc447a1764276
nitininhouse <nitin_g@me.iitr.ac.in>
2025-07-06 20:14:21 +0530

    add:header for mint page

---
+++ src/components/mint/MintHeader.tsx
@@ -0,0 +1,20 @@
+const MintHeader = () => (
+    <div className="bg-white border-b-8 border-black">
+      <div className="max-w-7xl mx-auto px-8 py-6">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <div className="bg-black text-white px-4 py-2 font-black text-lg border-4 border-black">
+              ZORA-CHAN
+            </div>
+            <div className="text-2xl font-black">CHARACTER COIN CREATOR</div>
+          </div>
+          <div className="bg-red-500 text-white px-4 py-2 font-black text-sm border-2 border-black transform rotate-3">
+            MINT COIN!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default MintHeader;
+  
