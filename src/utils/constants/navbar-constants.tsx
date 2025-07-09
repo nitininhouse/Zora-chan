de550232ac3beeb427ffb9a55e9f7f1eb928d066
nitininhouse <nitin_g@me.iitr.ac.in>
2025-07-09 11:29:47 +0530

    refractor: logo location

--- src/utils/constants/navbar-constants.tsx
+++ src/utils/constants/navbar-constants.tsx
@@ -1,5 +1,5 @@
-export const WEBSITE_NAME = "Smart Template";
-export const WEBSITE_DESCRIPTION = "A smart template for your Next.js projects.";
+export const WEBSITE_NAME = "ZORA-CHAN";
+export const WEBSITE_DESCRIPTION = "";
 export const WEBSITE_URL = "https://smart-template.example.com";
 export const GITHUB_REPO_URL = "https://github.com/username/smart-template";
 export const WEBSITE_TITLE_FONT = "Geist Sans, sans-serif";
